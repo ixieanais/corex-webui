6d52e248d2de140cb2a40192331af84a9e033266
Serhii Belous <hevneebelous@gmail.com>
2026-05-04 23:44:38 +0200

    delete server.py

--- server.py
+++
@@ -1,7 +0,0 @@
-from corex import app
-import uvicorn
-import webbrowser
-
-if __name__ == "__main__":
-    webbrowser.open("http://localhost:8080")
-    uvicorn.run(app=app, host="0.0.0.0", port=8080)
