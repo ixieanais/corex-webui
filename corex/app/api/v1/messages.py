8dc0b97f76f8e60e4db234870530aca0674a6820
ixieanais <92859414+ixieanais@users.noreply.github.com>
2026-05-08 22:36:35 +0200

    feat: add standart api for messages

--- corex/app/api/v1/messages.py
+++ corex/app/api/v1/messages.py
@@ -1,13 +1,30 @@
+from uuid import uuid4
+
 from fastapi import APIRouter
 
-import database
-from schemas import InsertUserMessageData
+from database import crud
+from schemas import MessageSchema
 
 
 router = APIRouter(tags=["messages"])
 
 
-@router.post("/insert_user_message")
-async def api_insert_user_message(data: InsertUserMessageData):
-    database.insert_user_message(data.id, data.message)
-    return {"id": data.id, "message": data.message}
+@router.post("/messages/{chat_id}")
+async def create_message(chat_id: str, schema: MessageSchema):
+    id = str(uuid4())
+    await crud.insert_message(id, chat_id, schema.ordinal, schema.role, schema.content)
+
+
+@router.get("/messages/{chat_id}")
+async def get_messages(chat_id: str):
+    return await crud.select_messages(chat_id)
+
+
+@router.patch("/messages/{id}")
+async def update_message(id: str, schema: MessageSchema):
+    await crud.update_message(id, schema.content)
+
+
+@router.delete("/messages/{id}")
+async def delete_message(id: str):
+    await crud.delete_message(id)
