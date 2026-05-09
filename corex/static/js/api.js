f4ce1b09f0db627f8aff87daae866f016439cdb9
ixieanais <92859414+ixieanais@users.noreply.github.com>
2026-05-09 18:24:11 +0200

    refac(api client): rename and reorganize

--- corex/static/js/api.js
+++ corex/static/js/api.js
@@ -1,42 +1,8 @@
-async function getChatsDB() {
-    const response = await fetch("/api/v1/chats", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
-    return await response.json().catch(() => {});
-}
-
-
-async function getModelsDB() {
-    const response = await fetch("/api/v1/models", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
-    return await response.json().catch(() => {});
-}
-
-
-async function chatCreateDB(chatName, message) {
+async function createChat(chatName) {
     const response = await fetch("/api/v1/chats", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
-        },
-        body: JSON.stringify({name: chatName, message: message})
-    });
-    return await response.json().catch(() => {});
-}
-
-
-async function chatRenameDB(chatId, chatName) {
-    const response = await fetch(`/api/v1/chats/${chatId}`, {
-        method: "PATCH",
-        headers: {
-            "Content-Type": "application/json",
         },
         body: JSON.stringify({name: chatName})
     });
@@ -44,45 +10,77 @@ async function chatRenameDB(chatId, chatName) {
 }
 
 
-async function chatDeleteDB(chatId) {
+async function getChats() {
+    const response = await fetch("/api/v1/chats", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    return await response.json().catch(() => {});
+}
+
+
+async function getChat(chatId) {
+    const response = await fetch(`/api/v1/chats/${chatId}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    return await response.json().catch(() => {});
+}
+
+
+async function updateChat(chatId, chatName) {
+    const response = await fetch(`/api/v1/chats/${chatId}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name: chatName})
+    });
+}
+
+
+async function deleteChat(chatId) {
     const response = await fetch(`/api/v1/chats/${chatId}`, {
         method: "DELETE",
         headers: {
             "Content-Type": "application/json"
         },
     });
-    return await response.json().catch(() => {});
 }
 
 
-async function getChatTitleDB(chatId) {
-    const response = await fetch(`/api/v1/chats/${chatId}/title`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
-    return await response.json();
-}
-
-
-async function getChatHistoryDB(chatId) {
-    const response = await fetch(`/api/v1/chats/${chatId}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
-    return await response.json();
-}
-
-
-async function insertUserMessageDB(chatId, message) {
-    const response = await fetch("/api/v1/insert_user_message", {
+async function createMessage(chatId, ordinal, role, content) {
+    const response = await fetch(`/api/v1/messages/${chatId}`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ id: chatId, message: message })
+        body: JSON.stringify({ordinal: ordinal, role: role, content: content})
     });
+}
+
+
+async function getMessages(chatId) {
+    const response = await fetch(`/api/v1/messages/${chatId}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    return await response.json();
+}
+
+
+async function getModels() {
+    const response = await fetch("/api/v1/models", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    return await response.json().catch(() => {});
 }
