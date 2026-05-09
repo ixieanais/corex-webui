9cbffdd60d5730fce519f5f58683f0ecb6f06d89
ixieanais <92859414+ixieanais@users.noreply.github.com>
2026-05-09 14:41:19 +0200

    refac: use crud module for chat database operations

--- corex/app/api/v1/chats.py
+++ corex/app/api/v1/chats.py
@@ -1,46 +1,35 @@
-import uuid
+from uuid import uuid4
 
 from fastapi import APIRouter
 
-import database
-from schemas import CreateChatData, RenameChatData
+from database import crud
+from schemas import ChatSchema
 
 
 router = APIRouter(tags=["chats"])
 
 
 @router.post("/chats")
-async def create_chat(data: CreateChatData):
-    chat_id = str(uuid.uuid4())
-    database.chat_create(chat_id, data.name, data.message)
-    return chat_id
+async def create_chat(schema: ChatSchema):
+    id = str(uuid4())
+    await crud.insert_chat(id, schema.name)
 
 
 @router.get("/chats")
 async def get_chats():
-    chats = database.get_chats()
-    return chats
+    return await crud.select_chats()
 
 
 @router.get("/chats/{id}")
 async def get_chat(id: str):
-    chat_history = database.get_chat_history(id)
-    return [{"role": r, "content": c} for r,c in chat_history]
-
-
-@router.get("/chats/{id}/title")
-async def api_get_chat_title(id: str):
-    title = database.get_chat_title(id)
-    return title
+    return await crud.select_chat(id)
 
 
 @router.patch("/chats/{id}")
-async def update_chat(id: str, data: RenameChatData):
-    database.chat_rename(id, data.name)
-    return {"id": id, "name": data.name}
+async def update_chat(id: str, schema: ChatSchema):
+    await crud.update_chat(id, schema.name)
 
 
 @router.delete("/chats/{id}")
 async def delete_chat(id: str):
-    database.chat_delete(id)
-    return {"id": id}
+    await crud.delete_chat(id)
